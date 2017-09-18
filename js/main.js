a83212aec149fef107475e78da9f3465ef63c8f1
Caity-G-OConnor <caity.g.oconnor@gmail.com>
2017-09-17 19:28:28 -0600

    Create function to comment on length of strings
    Follow instructions step 6b to create a function intended to log
    different messages based on whether the length of a string is less,
    more, or equal to 7 characters.

--- js/main.js
+++ js/main.js
@@ -55,3 +55,19 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else {
+        if (string.length > 7) {
+            console.log("I'm sorry, but that's too many to count.");
+        } else {
+            console.log('7, what a perfect choice!');
+        }
+    }
+};
+
+numba('7');
+numba('seventy');
+numba('university');
