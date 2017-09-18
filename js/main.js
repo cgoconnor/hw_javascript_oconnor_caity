9d1d74e1407fac871199ec1cbbf600349f847bd9
Caity-G-OConnor <caity.g.oconnor@gmail.com>
2017-09-17 19:03:20 -0600

    Create objects for last five US presidents
    Follow instructions step 5 to create objects containing basic
    information for the last five US presidents.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 var iceCream = ['mint chocolate chip', 'moose tracks', 'rocky road'];
 iceCream.push('raspberry chocolate chip');
+var barackObama = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};
+
+var georgeWBush = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+
+var billClinton = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};
+
+var georgeBush = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 1,
+    party: 'Republican',
+    yearsOfPresidency: 4
+};
+
+var ronaldReagan = {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
